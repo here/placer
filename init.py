b36908540a91d19c2371042b7e21d946c7c7948f
mike@here <mike@herebox.org>
2024-08-11 12:41:18 -0700

    working example googleapis maps place textsearch

---
+++ init.py
@@ -0,0 +1,26 @@
+import requests
+
+api_key = "AIzaSyBPsgrtioZSfPYnnKPKZP2SGjmwLQ86Ego"
+
+def get_place_info(address, api_key):
+# Base URL
+  base_url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+# Parameters in a dictionary
+  params = {
+   "query": address,
+   # "inputtype": "textquery",
+   # "fields": "formatted_address,name,business_status,place_id",
+   # "fields": "*",
+   "key": api_key,
+  }
+# Send request and capture response
+  response = requests.get(base_url, params=params)
+# Check if the request was successful
+  if response.status_code == 200:
+    return response.json()
+  else:
+    return None
+
+print("end of line")
+
+print(get_place_info("Cafe Tropical, 2900 Sunset Blvd, Los Angeles, CA 90026", api_key))
