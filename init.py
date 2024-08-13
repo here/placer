8cf178cd62c960312660013f585a0345f367f899
mike@here <mike@herebox.org>
2024-08-13 14:15:52 -0700

    init comments cleanup

--- init.py
+++ init.py
@@ -8,49 +8,42 @@ import pandas as pd
 load_dotenv()
 
 api_key = os.getenv('API_KEY')
-inputfile = 'input.xslx'
-outputfile = 'output.json'
+inputfile = 'out.xlsx'
 
 def get_place_info(address, api_key):
-# Base URL
   base_url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
-# Parameters in a dictionary
+  # Parameters in a dictionary
   params = {
    "query": address,
-   # "inputtype": "textquery",
-   # "fields": "formatted_address,name,business_status,place_id",
-   # "fields": "*",
    "key": api_key,
   }
-# Send request and capture response
+  # Send request and capture response
   response = requests.get(base_url, params=params)
-# Check if the request was successful
+  # Check if the request was successful
   if response.status_code == 200:
     return response.json()
   else:
     return None
 
-print("API Key")
-print(api_key)
+print(f"API Key {api_key}")
 
-df = pd.read_excel('out.xlsx')
-# df['maps'] = 'empty'
+# Read in dataframe as df from excel using pandas as pd
+df = pd.read_excel(inputfile)
 
 searchdf = df[['Name','Address','City','St','Zip','mapobj']]
 
+# Placeholder name and address
 # name = "Cafe Tropical"
 # address = "2900 Sunset Blvd, Los Angeles, CA 90026"
 
-# name = searchtext[0]['Name']
-
-start = 42 # Start row number in excel
+start = 0 # Start row number in excel
 loops = 60
 reqcount = 0
 for i, row in enumerate(searchdf.itertuples()):
   if i+2 < start: # Add two for index to match excel sheet row number.  Adds header row and first result zero row.
     print(f"{i} continuing not yet {start}")
     continue
-  # print(row)
+
   searchrow = f"{row.Name}, {row.Address}, {row.City}, {row.St}, {row.Zip}"
   print(searchrow)
 
@@ -71,6 +64,7 @@ for i, row in enumerate(searchdf.itertuples()):
 
   df.at[row.Index,'mapresults'] = len(response['results'])
   
+  # Responses with exactly one result are more reliable for breaking out data
   if len(response['results']) == 1:
     df.at[row.Index,'mapstatus'] = response['results'][0].get('business_status')
     df.at[row.Index,'mapname'] = response['results'][0].get('name')
@@ -79,6 +73,7 @@ for i, row in enumerate(searchdf.itertuples()):
     df.at[row.Index,'mapratecount'] = response['results'][0].get('user_ratings_total')
     df.at[row.Index,'maptypes'] = json.dumps(response['results'][0].get('types'))
   
+  # Store full response python object and json for future use
   df.at[row.Index,'mapobj'] = response
   df.at[row.Index,'mapjson'] = json.dumps(response)
 
@@ -91,6 +86,4 @@ for i, row in enumerate(searchdf.itertuples()):
 print(df.head)
 
 df.to_excel('./new.xlsx',engine="openpyxl")
-#df.to_excel('./out.xlsx')
-
-# print(places)
+# df.to_excel('./out.xlsx') # Permissions error to overwrite existing file
